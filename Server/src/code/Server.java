29e5a12ee40ec6a276823bca88d40bb7b184b295
kiiiu15 <tinbulzomi@hotmail.com>
2020-06-16 23:41:39 -0300

    Creation of Server constructor and server socket
    When a new servr is created, we open aserver sockets on port 3000 as requested.

--- Server/src/code/Server.java
+++ Server/src/code/Server.java
@@ -1,9 +1,33 @@
 package code;
 
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * This class will be one who open a socket server on a port and accepts conection from costumers
+ * Each costumer is attended in a separated thread to handle multiple clients
+ */
 public class Server {
 
+    private final ServerSocket serverSocket;
+    private final Integer PORT = 3000; //port that was requested
+
+    /**
+     * We create a serverSocket in the designated port
+     * @throws IOException if anythings goes wrong.
+     */
+    public Server() throws IOException {
+        this.serverSocket = new ServerSocket(PORT);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Server server = new Server();
+        } catch (IOException e) {
+            System.out.println("There was a problem setting the server up");
+        }
     }
 
 }
