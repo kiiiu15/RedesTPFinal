6c49c2be0d1bfad2ee5dec83d83fe2543250c96f
kiiiu15 <tinbulzomi@hotmail.com>
2020-06-17 02:03:43 -0300

    Creation of socket
    In order to make it configurables, the host and ip and passed by constructor parameters and the clients is a thread becase we can have many at the  same time

--- Client/src/code/Client.java
+++ Client/src/code/Client.java
@@ -1,4 +1,18 @@
 package code;
 
-public class Client {
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client extends Thread {
+
+    private final Socket socket;
+
+    public Client(String host, Integer port) throws IOException {
+        socket = new Socket(host, port);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Soy el cliente y estoy corriendo");
+    }
 }
